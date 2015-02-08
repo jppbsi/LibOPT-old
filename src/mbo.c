d3ef5043090651986913fa10ae4544a111bd96b9
jppbsi <papa.joaopaulo@gmail.com>
2015-02-07 22:46:07 -0200

    fixed bugs @CreateBirdFlock and @DestroyBirdFlock

--- src/mbo.c
+++ src/mbo.c
@@ -6,7 +6,7 @@ m: number of birds
 n: number of decision variables */
 BirdFlock *CreateBirdFlock(int m, int n){
 
-	if(m < 1){
+	if(m < 1 || n < 1){
 		fprintf(stderr,"\nInvalid parameters @CreateFlockBird.\n");
 		return NULL;
 	}
@@ -16,18 +16,20 @@ BirdFlock *CreateBirdFlock(int m, int n){
 	B = (BirdFlock *)malloc(sizeof(BirdFlock));
 	B->m = m;
 	B->n = n;
-
+	
 	B->k = 0;
 	B->M = 0;
 	B->max_iterations = 0;
 	B->X = 0;
 	B->leader = 0;
-	B->x = gsl_matrix_alloc(B->m, B->n);
+	B->x = gsl_matrix_calloc(B->m, B->n);
 	B->fitness = gsl_vector_alloc(B->m);
 	B->left = (int *)malloc(ceil(B->m-1)/2.0);
 	B->right = (int *)malloc(B->m/2);
 	B->LB = gsl_vector_alloc(B->n);
 	B->UB = gsl_vector_alloc(B->n);
+	
+	return B;
 }
 
 /* It deallocates the birdflock ---
@@ -39,8 +41,8 @@ void DestroyBirdFlock(BirdFlock **B){
 	if(aux){
 		gsl_matrix_free(aux->x);
 		gsl_vector_free(aux->fitness);
-		gsl_vector_free(aux->left);
-		gsl_vector_free(aux->right);
+		free(aux->left);
+		free(aux->right);
 		gsl_vector_free(aux->LB);
 		gsl_vector_free(aux->UB);
 		free(aux);
